3238d1632e85bec1b35224b833d836f80399ab27
Sebastian Carrillo <scarrilloh77@gmail.com>
2023-07-17 12:25:02 -0500

    Select test cases

--- packages/react/src/molecules/Select/Select.test.tsx
+++ packages/react/src/molecules/Select/Select.test.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Select from './Select';
 
-import { render, fireEvent } from '@testing-library/react';
+import { fireEvent, render } from '@testing-library/react';
 
 const options = [
   {
@@ -24,9 +24,67 @@ test('renders all options passed to it', () => {
   expect(getAllByRole('menuitemradio')).toHaveLength(options.length);
 });
 
-test('renders options using custom renderOption method if passed as prop', () => {});
-test('calls the onOptionSelected prop with the selected option and its index if passed', () => {});
-test('the button label changes to the selected option label', () => {});
-test('snapshot of the selected option state', () => {});
-test('snapshot of the base state', () => {});
-test('snapshot of the options menu open state', () => {});
+test('renders options using custom renderOption method if passed as prop', () => {
+  const { getByTestId, getAllByTestId } = render(
+    <Select
+      options={options}
+      renderOption={({ option, getOptionRecommendedProps }) => (
+        <li data-testid="CustomRenderOption" {...getOptionRecommendedProps()}>
+          {option.value}
+        </li>
+      )}
+    />
+  );
+  fireEvent.click(getByTestId('DseSelectButton'));
+  expect(getAllByTestId('CustomRenderOption')).toHaveLength(options.length);
+});
+
+test('calls the onOptionSelected prop with the selected option and its index if passed', () => {
+  const onOptionSelected = jest.fn();
+  const { getByTestId, getAllByRole } = render(
+    <Select
+      options={options}
+      onOptionSelected={onOptionSelected}
+      renderOption={({ option, getOptionRecommendedProps }) => (
+        <p {...getOptionRecommendedProps()}>{option.label}</p>
+      )}
+    />
+  );
+  fireEvent.click(getByTestId('DseSelectButton'));
+  fireEvent.click(getAllByRole('menuitemradio')[2]);
+  expect(onOptionSelected).toHaveBeenCalledWith(options[2], 2);
+});
+
+test('the button label changes to the selected option label', () => {
+  const { getAllByRole, getByTestId } = render(<Select options={options} />);
+  fireEvent.click(getByTestId('DseSelectButton'));
+  fireEvent.click(getAllByRole('menuitemradio')[2]);
+  expect(getByTestId('DseSelectButton')).toHaveTextContent(options[2].label);
+});
+
+test('snapshot of the selected option state', () => {
+  const { getAllByRole, getByTestId, asFragment } = render(
+    <Select options={options} />
+  );
+  fireEvent.click(getByTestId('DseSelectButton'));
+  fireEvent.click(getAllByRole('menuitemradio')[2]);
+  expect(asFragment()).toMatchSnapshot();
+});
+
+test('snapshot of the base state', () => {
+  const { asFragment } = render(<Select options={options} />);
+  expect(asFragment()).toMatchSnapshot();
+});
+
+test('snapshot of the options menu open state', () => {
+  const { getByTestId, asFragment } = render(<Select options={options} />);
+  fireEvent.click(getByTestId('DseSelectButton'));
+  expect(asFragment()).toMatchSnapshot();
+});
+
+test('can customize select label', () => {
+  const { getByText } = render(
+    <Select options={options} label="THIS IS A CUSTOM LABEL" />
+  );
+  expect(getByText(/THIS IS A CUSTOM LABEL/)).toBeInTheDocument();
+});
